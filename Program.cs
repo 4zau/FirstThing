635be3e2f5f560130cadf709ad6f3fa942bf8140
4zau <4zau@mail.ru>
2022-05-09 10:59:02 +0700

    created smth

--- Program.cs
+++ Program.cs
@@ -5,5 +5,16 @@ class Hello
     static void Main()
     {
         Console.WriteLine("Hello, World");
+        Console.Write("enter a door from 1 to 3: ");
+        string door = Console.ReadLine();
+        if (door == "1") {
+            Console.WriteLine("you died lol");
+        } else if (door == "2") {
+            Console.WriteLine("you died lol");
+        } else if (door == "3") {
+            Console.WriteLine("you died lol");
+        } else {
+            Console.WriteLine("you lived");
+        }
     }
 }

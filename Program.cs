8b67dfb99b668478aff27bf65d93083afab33111
4zau <4zau@mail.ru>
2022-05-09 11:51:46 +0700

    Tasking

--- Program.cs
+++ Program.cs
@@ -1,20 +1,53 @@
 ﻿using System;
 
-class Hello
-{
-    static void Main()
-    {
-        Console.WriteLine("Hello, World");
-        Console.Write("enter a door from 1 to 3: ");
-        string door = Console.ReadLine();
-        if (door == "1") {
-            Console.WriteLine("you died lol");
-        } else if (door == "2") {
-            Console.WriteLine("you died lol");
-        } else if (door == "3") {
-            Console.WriteLine("you died lol");
-        } else {
-            Console.WriteLine("you lived");
+namespace FirstThing {
+    class Hello {
+        static void Main() {
+            List<Task> tasks = new List<Task>() {
+                new Task() {EstimatedTime = 3, Status = Status.NotStarted},
+                new Task() {EstimatedTime = 1, Status = Status.InProgress},
+                new Task() {EstimatedTime = 0, Status = Status.Done},
+                new Task() {EstimatedTime = 2, Status = Status.OnHold},
+            };
+            int currentTask = 1;
+            foreach (Task task in tasks) {
+                Console.WriteLine("Current task: " + currentTask.ToString());
+                switch (task.Status) {
+                    case Status.Done:
+                        Console.WriteLine("Already done!");
+                        break;
+                    case Status.InProgress:
+                        Console.WriteLine("Already in progress, time left: " + task.EstimatedTime.ToString());
+                        Console.WriteLine("Finishing...");
+                        System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
+                        Console.WriteLine("Done!");
+                        break;
+                    case Status.NotStarted:
+                        Console.WriteLine("Starting task, time left: " + task.EstimatedTime.ToString());
+                        Console.WriteLine("In progress...");
+                        System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
+                        Console.WriteLine("Done!");
+                        break;
+                    case Status.OnHold:
+                        Console.WriteLine("This task is on hold, skipping...");
+                        break;
+                }
+                Console.WriteLine("Moving onto next task...");
+                currentTask++;
+            }
+            Console.WriteLine("Everything is done!");
         }
+    }
+
+    class Task {
+        public int EstimatedTime { get; set; }
+        public Status Status { get; set; }
+    }
+
+    enum Status {
+        Done,
+        OnHold,
+        InProgress,
+        NotStarted
     }
 }

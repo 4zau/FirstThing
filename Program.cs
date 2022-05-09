bb61da43fe8be904fe61745110c100679a9743f5
4zau <4zau@mail.ru>
2022-05-09 13:33:11 +0700

    hub added

--- Program.cs
+++ Program.cs
@@ -3,51 +3,21 @@
 namespace FirstThing {
     class Hello {
         static void Main() {
-            List<Task> tasks = new List<Task>() {
-                new Task() {EstimatedTime = 3, Status = Status.NotStarted},
-                new Task() {EstimatedTime = 1, Status = Status.InProgress},
-                new Task() {EstimatedTime = 0, Status = Status.Done},
-                new Task() {EstimatedTime = 2, Status = Status.OnHold},
-            };
-            int currentTask = 1;
-            foreach (Task task in tasks) {
-                Console.WriteLine("Current task: " + currentTask.ToString());
-                switch (task.Status) {
-                    case Status.Done:
-                        Console.WriteLine("Already done!");
+            bool shouldClose = false;
+            while (!shouldClose) {
+                Console.WriteLine("What do you want?");
+                Console.WriteLine("1. Tasking");
+                Console.WriteLine("0. Quit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice) {
+                    case 1:
+                        Tasking.Execute();
                         break;
-                    case Status.InProgress:
-                        Console.WriteLine("Already in progress, time left: " + task.EstimatedTime.ToString());
-                        Console.WriteLine("Finishing...");
-                        System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
-                        Console.WriteLine("Done!");
-                        break;
-                    case Status.NotStarted:
-                        Console.WriteLine("Starting task, time left: " + task.EstimatedTime.ToString());
-                        Console.WriteLine("In progress...");
-                        System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
-                        Console.WriteLine("Done!");
-                        break;
-                    case Status.OnHold:
-                        Console.WriteLine("This task is on hold, skipping...");
+                    case 0:
+                        shouldClose = true;
                         break;
                 }
-                Console.WriteLine("Moving onto next task...");
-                currentTask++;
             }
-            Console.WriteLine("Everything is done!");
         }
-    }
-
-    class Task {
-        public int EstimatedTime { get; set; }
-        public Status Status { get; set; }
-    }
-
-    enum Status {
-        Done,
-        OnHold,
-        InProgress,
-        NotStarted
     }
 }
---
+++ Tasking.cs
@@ -0,0 +1,49 @@
+namespace FirstThing; 
+
+public class Tasking {
+    public static void Execute() {
+        List<Task> tasks = new List<Task>() {
+            new Task() {EstimatedTime = 3, Status = Status.NotStarted},
+            new Task() {EstimatedTime = 1, Status = Status.InProgress},
+            new Task() {EstimatedTime = 0, Status = Status.Done},
+            new Task() {EstimatedTime = 2, Status = Status.OnHold},
+        };
+        int currentTask = 1;
+        foreach (Task task in tasks) {
+            Console.WriteLine("Current task: " + currentTask.ToString());
+            switch (task.Status) {
+                case Status.Done:
+                    Console.WriteLine("Already done!");
+                    break;
+                case Status.InProgress:
+                    Console.WriteLine("Already in progress, time left: " + task.EstimatedTime.ToString());
+                    Console.WriteLine("Finishing...");
+                    System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
+                    Console.WriteLine("Done!");
+                    break;
+                case Status.NotStarted:
+                    Console.WriteLine("Starting task, time left: " + task.EstimatedTime.ToString());
+                    Console.WriteLine("In progress...");
+                    System.Threading.Thread.Sleep(task.EstimatedTime * 1000);
+                    Console.WriteLine("Done!");
+                    break;
+                case Status.OnHold:
+                    Console.WriteLine("This task is on hold, skipping...");
+                    break;
+            }
+            Console.WriteLine("Moving onto next task...");
+            currentTask++;
+        }
+        Console.WriteLine("Everything is done!");
+    }
+    class Task {
+        public int EstimatedTime { get; set; }
+        public Status Status { get; set; }
+    }
+    enum Status {
+        Done,
+        OnHold,
+        InProgress,
+        NotStarted
+    }
+}
